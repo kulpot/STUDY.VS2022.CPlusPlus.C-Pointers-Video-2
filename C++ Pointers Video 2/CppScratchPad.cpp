6e1b51735f35a9ae63af955903fd3899fa4e3fef
Kulpot <kulpotwahu@gmail.com>
2023-12-20 18:03:02 +0800

    Add project files.

---
+++ C++ Pointers Video 2/CppScratchPad.cpp
@@ -0,0 +1,46 @@
+#include <cassert>
+#include <iostream>
+using std::cout;
+using std::endl;
+
+// ---------------------------------- C++ Pointers Video 2 --------------------------------------------------
+// ref link:https://www.youtube.com/watch?v=DSeBneAyDx4&list=PLRwVmtr-pp05AW78n3IkMJ3M1dlQ-wgv1&index=2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------- C++ Pointers Introduction --------------------------------------------
+// ref link:https://www.youtube.com/watch?v=lAHgavXXWXM&list=PLRwVmtr-pp05AW78n3IkMJ3M1dlQ-wgv1
+
+// C++ predecessor to java and C#
+
+void main() // functions
+{
+	int myVar = 23; // store in the memory stack
+	int *myPointer = &myVar;		// & - means what is the address of
+	//int yourInt, * yourIntPointer;
+	//int* myPointer;
+	
+	// myPointer ---> myVar
+	cout << myPointer << endl;    // 00000066DE56FC14 - address of myVar;
+	cout << (*myPointer) << endl;		// output value: 23
+	(*myPointer) = 15;
+	cout << myVar << endl;			// output modified value: 15
+}
