49ae4e1cb1e3d8e7a1299332513a0cc572c93ae6
Kulpot <kulpotwahu@gmail.com>
2023-12-20 18:05:35 +0800

    ...

--- C++ Pointers Video 2/CppScratchPad.cpp
+++ C++ Pointers Video 2/CppScratchPad.cpp
@@ -40,16 +40,16 @@ void main()
 
 // C++ predecessor to java and C#
 
-void main() // functions
-{
-	int myVar = 23; // store in the memory stack
-	int *myPointer = &myVar;		// & - means what is the address of
-	//int yourInt, * yourIntPointer;
-	//int* myPointer;
-	
-	// myPointer ---> myVar
-	cout << myPointer << endl;    // 00000066DE56FC14 - address of myVar;
-	cout << (*myPointer) << endl;		// output value: 23
-	(*myPointer) = 15;
-	cout << myVar << endl;			// output modified value: 15
-}
+//void main() // functions
+//{
+//	int myVar = 23; // store in the memory stack
+//	int *myPointer = &myVar;		// & - means what is the address of
+//	//int yourInt, * yourIntPointer;
+//	//int* myPointer;
+//	
+//	// myPointer ---> myVar
+//	cout << myPointer << endl;    // 00000066DE56FC14 - address of myVar;
+//	cout << (*myPointer) << endl;		// output value: 23
+//	(*myPointer) = 15;
+//	cout << myVar << endl;			// output modified value: 15
+//}
